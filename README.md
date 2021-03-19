d65c42716ad67de60b5b82dd81c691e1d27e960d
Zahrah Rahmani Putri <77628808+zahrahrp@users.noreply.github.com>
2021-03-19 13:26:34 +0700

    doing W03 assignment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # os211
 Operating Systems 2021-1 (Semester Genap 2020/2021). <br>
-Zahrah Rahmani Putri's [Repository](https://github.com/zahrahrp/os211) | [Page](https://zahrahrp.github.io/os211/) |  [Log](https://zahrahrp.github.io/os211/TXT/mylog.txt)
+Zahrah Rahmani Putri's [Repository](https://github.com/zahrahrp/os211) | [Page](https://zahrahrp.github.io/os211/) | [Key](https://zahrahrp.github.io/os211/TXT/mypubkey.txt) | [Log](https://zahrahrp.github.io/os211/TXT/mylog.txt)
 
 ## Weekly Top 10 List
 [W01](W01/) |
--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -6,13 +6,18 @@
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+# REV04 Mon 15 Mar 19:27:52 WIB 2021
 # REV03 Sun 14 Mar 18:21:27 WIB 2021
 # REV02 Fri 12 Mar 13:40:58 WIB 2021
 # REV01 Tue 13 Oct 10:37:14 WIB 2020
 # START Mon 28 Sep 21:05:04 WIB 2020
 
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
+
+REC2="rahmanizahrah@gmail.com"
 REC1="operatingsystems@vlsm.org"
-REC2="cbk@dummy"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
 
